10e4fd7d93e8a0e827cfff7a23ed4806ccef11ca
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2021-03-08 13:03:43 -0500

    ADD: mongodb

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const { connectDB } = require('./config/db');
 const app = express();
 
 dotenv.config();
+
+connectDB();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.get('/*', (req, res) => {
+  res.status(404).json({ message: 'This page does not exists' });
+});
 
 const port = process.env.PORT || 3030;
 
